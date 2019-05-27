ce33529b9800c1560282f182525b025c5d1299dc
foxtrot <foxtrot@pdnk.org>
2019-05-27 21:57:31 +0700

    Initial Commit

---
+++ NewCarList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewCarList
+//
+//  Created by foxtrot on 27/05/2019.
+//  Copyright © 2019 foxtrot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
